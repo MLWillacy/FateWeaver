d357a583e9bad3213c604f54d37e0affc8436d99
MLWillacy <71636850+MLWillacy@users.noreply.github.com>
2023-01-26 01:41:32 +0000

    Added classes for unit and weapon

---
+++ Fate1/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fate1
+{
+    internal class Unit
+    {
+        private int mModelCount;
+        private int mWounds;
+        private int mMovement;
+        private int mBravery;
+        private int mSave;
+
+    }
+}
---
+++ Fate1/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fate1
+{
+    internal class Weapon
+    {
+        private string mWeaponType; //either "missile" or "melee"
+        private string mWeaponName;
+        private int mAttacks;
+        private int mRange;
+        private int mToHit;
+        private int mToWound;
+        private int mRend;
+        private int mDamage;
+
+    }
+}
